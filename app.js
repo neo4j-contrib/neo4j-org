b1de84ea0cfabe42e399d76c97128143d1e4c054
Andreas Kollegger <andreas@neotechnology.com>
2012-11-04 17:27:30 -0800

    return to randmozed theme style; added aqau theme to replicate current neo4j.org;

--- app.js
+++ app.js
@@ -32,10 +32,9 @@ app.configure('development', function(){
 
 var rnd = function rnd(min, max) { return Math.floor(Math.random() * (max - min + 1)) + min; }
 
-var themes = ["default","quartum","coolblue"];
+var themes = ["default","quartum","coolblue","aqua"];
 app.locals.theme = function() {
-/*   return themes[rnd(0,themes.length - 1)]; */
-  return "coolblue";
+  return themes[rnd(0,themes.length - 1)];
 }
  
 app.locals({
