de9530d3f640c1aa17784637e675bab656bd5b70
Michael Hunger <github@jexp.de>
2012-11-28 23:58:57 +0100

    forwarder with special handling for foo_ urls

--- forwarder.js
+++ forwarder.js
@@ -1,88 +1,89 @@
 var express = require('express')
   , http = require('http');
 
-exports.add_console_forward=function(app,express,http) {
+exports.add_console_forward=function(app) {
 
-    var host = "console-test.neo4j.org"; // "console-test.neo4j.org";
-    var port = 80; // 80;
+    var target = process.env.NEO4J_CONSOLE || "console-test.neo4j.org:80"
+    var host =  target.split(":")[0];
+    var port = target.split(":")[1];
 
-    String.prototype.startsWith = function(str) { return this.substr(0,str.length)==str;}
     app.use(function(req, res, next) {
         var path = req.path;
         // console.log('app.use rawbody '+path);
         if (!path.match("^/console")) {
             return next();
-            }
+        }
         if (path=="/console") return console_forward(req,res,"/");
+        if (path.match("_$")) return console_forward(req,res,path.substring(0,path.length-1));
 
         var m=path.match("^/console/r/(.+)$");
         if (m) return console_forward(req,res,"/?id="+m[1]);
         console_forward(req,res,path);
     });
 
-    function console_forward(request, response, path) {
+    function console_forward(sourceRequest, sourceResponse, path) {
         try {
-            path = path || request.path;
+            path = path || sourceRequest.path;
             // console.log("host: " + host+ " port "+port);
             // console.log("path: " + path);
-            // console.log("querystring: " + request.query);
+            // console.log("querystring: " + sourceRequest.query);
 
             var headers = {connection: 'keep-alive'};
-            for (var key in request.headers) {
+            for (var key in sourceRequest.headers) {
                 if (key!="host") {
-                    headers[key] = request.headers[key];
+                    headers[key] = sourceRequest.headers[key];
                 }
             }
             // console.log("header: " + JSON.stringify(headers));
 
-            var req = http.request(
-                {host: host, port:port, method:request.method || "GET",
+            var targetRequest = http.request(
+                {host: host, port:port, method:sourceRequest.method || "GET",
                     headers: headers, path: path},
 
-                function(res) {
-                    // console.log('STATUS: ' + res.statusCode);
-                    // console.log('HEADERS: ' + JSON.stringify(res.headers));
-                    response.status(res.statusCode);
-                    for (var key in res.headers) {
-                        response.header(key, res.headers[key]);
-                        // console.log(key + ": " + res.headers[key])
+                function(targetResponse) {
+                    // console.log('STATUS: ' + targetResponse.statusCode);
+                    // console.log('HEADERS: ' + JSON.stringify(targetResponse.headers));
+                    sourceResponse.status(targetResponse.statusCode);
+                    for (var key in targetResponse.headers) {
+                        sourceResponse.header(key, targetResponse.headers[key]);
+                        // console.log(key + ": " + targetResponse.headers[key])
                     }
                     var data='';
-                    var isText=!res.headers['content-type'] || res.headers['content-type'].match("^text");
-                    res.setEncoding(isText ? 'utf-8' : 'binary');
-                    res.on('data', function (chunk) {
+                    var isText=!targetResponse.headers['content-type'] || targetResponse.headers['content-type'].match("^text");
+                    targetResponse.setEncoding(isText ? 'utf-8' : 'binary');
+                    targetResponse.on('data', function (chunk) {
                         data += chunk;
-                        // console.log("res content-type " + response.get('Content-Type'));
+                        // console.log("targetResponse content-type " + sourceResponse.get('Content-Type'));
                         // console.log('BODY: ' + (typeof(chunk)) + " offset " + (data ? data.offset : "no data") + " length  " + chunk.length + " " + (typeof(chunk) == 'string' ? chunk.substr(0, 100) + "\n" + chunk.substr(chunk.length - 100) + "\n" : ""));
                     });
-                    res.on("end", function(err) {
+                    targetResponse.on("end", function(err) {
                         // console.log("END: " + err + " offset " + (data ? data.offset : "no data") + " len: " + (data ? data.length : "no data")); // +data.toString()
                         if (data && data.length) {
-                            response.send(isText ? data : new Buffer(data, 'binary'));
+                            sourceResponse.send(isText ? data : new Buffer(data, 'binary'));
                         }
-                        response.end();
-                        // console.log("res content-type " + response.get('Content-Type'))
+                        sourceResponse.end();
+                        // console.log("targetResponse content-type " + sourceResponse.get('Content-Type'))
                     })
                 });
 
-            req.on('error', function(e) {
-                // console.log('problem with request: ' + e.message);
-                response.send(500, e);
+            targetRequest.on('error', function(e) {
+                // console.log('problem with sourceRequest: ' + e.message);
+                sourceResponse.send(500, e);
             });
 
 
-            request.on('data', function(chunk) {
+            sourceRequest.on('data', function(chunk) {
                 // console.log('app.use rawbody chunk: ' + chunk);
-                req.write(chunk);
+                targetRequest.write(chunk);
             });
 
 
-            request.on('end', function() {
-                // console.log("request.content-length " + request.header("content-length"));
-                req.end();
+            sourceRequest.on('end', function() {
+                // console.log("sourceRequest.content-length " + sourceRequest.header("content-length"));
+                targetRequest.end();
             });
         } catch(e) {
-            response.send(500, e);
+            sourceResponse.send(500, e);
         }
     }
 }
