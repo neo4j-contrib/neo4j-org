bb8607ed90981eb61ed1c4dc60050a337b0041bb
Michael Hunger <github@jexp.de>
2012-12-10 16:08:18 +0100

    updated svg graph, marketo id token

--- public/assets/js/neo4j.js
+++ public/assets/js/neo4j.js
@@ -11,6 +11,27 @@ $(document).ready(function(){
 });
 
 
+function getCookies() {
+    var cks = {};
+    var ckList = document.cookie.split("; ");
+    for (var i=0; i < ckList.length; i++)
+    {
+        var ck = ckList[i].split("=");
+        cks[ck[0]] = unescape(ck[1]);
+    }
+    return cks;
+}
+
+function marketo_cookie() {
+    Munchkin.createTrackingCookie(true);
+    var cookies=document.cookie.split("; ");
+    for (i in cookies) {
+        var m = cookies[i].match(/^_mkto_trk=.+-(\d+)$/);
+        if (m) return m[1];
+    }
+    return null;
+}
+
 function renderConsole(url) {
     $('#console').html('<a class="btn" href="'+url+'" target="_blank">Open in new Window</a><iframe width="800" height="400" src="'+url+'"/>');
 }
