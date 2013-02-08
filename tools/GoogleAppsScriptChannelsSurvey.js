b068df33fe323ee9c50106124b5c70cba8b2e7ef
peterneubauer <peter@neubauer.se>
2013-02-08 12:38:17 +0100

    makign voting work with additions

--- tools/GoogleAppsScriptChannelsSurvey.js
+++ tools/GoogleAppsScriptChannelsSurvey.js
@@ -3,31 +3,38 @@
 //POST https://script.google.com/a/macros/neopersistence.com/s/AKfycbyqIsN-_D94_406DM1qjSc5GyktWf6miAscmZ-hhVEvQ59QZQO2/exec?voteRow=10
 
 function doPost(e) {
-    Logger.log('Polls form submitted with vote for ' + Logger.log('Polls application reached, accessing with parameters = ' + e.parameter.voteRow));
-    var ss = SpreadsheetApp.openByUrl("https://docs.google.com/a/neopersistence.com/spreadsheet/ccc?key=0AvhGAQ-et4qYdDV0dnVUQVhnU1hJTjhTeXFrbWlnR3c");
-    var sheet = ss.getSheets()[0];
-    Logger.log("sheet:"+sheet);
-    if(e.parameter.voteRow!=undefined) {
-        var cell=sheet.getRange(e.parameter.voteRow,2,1,1);
-        Logger.log("sheet:"+cell);
-        var new_votes=cell.getValue()+1;
-        cell.setValue(new_votes);
+    try {
+        Logger.log(new Date() + 'Polls form submitted');
+        var ss = SpreadsheetApp.openByUrl("https://docs.google.com/a/neopersistence.com/spreadsheet/ccc?key=0AvhGAQ-et4qYdDV0dnVUQVhnU1hJTjhTeXFrbWlnR3c");
+        var sheet = ss.getSheets()[0];
+        Logger.log("sheet:"+sheet+ " voteRow" + e.parameter.voteRow);
+        if(e.parameter.voteRow!=undefined) {
+            var cell=sheet.getRange(e.parameter.voteRow,2,1,1);
+            Logger.log("sheet:"+cell);
+            var new_votes=cell.getValue()+1;
+            cell.setValue(new_votes);
+        }
+        var addRow = e.parameter.addRow;
+        Logger.log("sheet:"+sheet+ " addRow" + addRow);
+        if(addRow) {
+            var nrOfRows = sheet.getDataRange().getNumRows();
+            Logger.log("adding row:"+e.parameter.addRow+" "+ nrOfRows);
+            var i = nrOfRows+1;
+            sheet.insertRowAfter(nrOfRows);
+            sheet.getRange(i,1,1,1).setValue(e.parameter.addRow);
+            sheet.getRange(i,2,1,1).setValue(1);
+            sheet.getRange(i,3,1,1).setValue(e.parameter.url);
+            sheet.getRange(i,4,1,1).setValue(e.parameter.logourl);
+            sheet.getRange(i,5,1,1).setValue(e.parameter.language);
+            return Logger.getLog();
+
+        }
+    } catch (e) {
+        Logger.log(e);
     }
-
-    if(e.parameter.addRow!=undefined) {
-        var cells=sheet.getDataRange();
-        Logger.log("adding row:"+row);
-        var row = e.parameter.addRow;
-        Logger.log("rows:"+cells.getNumRows());
-        sheet.insertRowAfter(cells.getNumRows());
-        Logger.log("rows:"+cells.getNumRows());
-        sheet.getRange(cells.getNumRows()+1,1,1,1).setValue(row);
-        sheet.getRange(cells.getNumRows()+1,2,1,1).setValue(1);
-        sheet.getRange(cells.getNumRows()+1,3,1,1).setValue(e.parameter.url);
-        sheet.getRange(cells.getNumRows()+1,4,1,1).setValue(e.parameter.logourl);
-        sheet.getRange(cells.getNumRows()+1,5,1,1).setValue(e.parameter.language);
-
-    }
-
-    return "ok";
 }
+
+function testPost() {
+    return doPost({parameter:{addRow:"test"}});
+}
+       
