44d37317c58ecb085e2972883ddd5747bb05e65e
Michael Hunger <github@jexp.de>
2013-03-07 05:38:36 +0100

    fix sdn driver

--- helpers/pages.js
+++ helpers/pages.js
@@ -283,7 +283,7 @@ exports.pages = {
         introText: "Friends of Neo4j speak many languages, and work in many frameworks.",
         thumbnail: "/assets/img/still/neo4j_drivers.png",
         featured: null,
-        related: ["neo4j_rest", "spring", "neo4j_java", "neo4j_rb", "neography", "neo4jphp", "neo4jclient", "py2neo", "neo4j_python", "node_neo4j", "neocons", "bulbflow", "keymaker", "neoid", "neo4django", "neo4j_rest_client", "neo4p", "scala", "grails", "anorm_cypher", "haskell", "datanucleus", "neo4j_go"]
+        related: ["neo4j_rest", "spring_data_neo4j", "neo4j_java", "neo4j_rb", "neography", "neo4jphp", "neo4jclient", "py2neo", "neo4j_python", "node_neo4j", "neocons", "bulbflow", "keymaker", "neoid", "neo4django", "neo4j_rest_client", "neo4p", "scala", "grails", "anorm_cypher", "haskell", "datanucleus", "neo4j_go"]
     },
     tools: {
         path: "/develop/tools",
