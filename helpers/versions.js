8ddd9686fb58a869c2316626bfa4b16a28fdf7f0
Michael Hunger <github@jexp.de>
2013-05-21 03:43:34 +0200

    release version fixes some fallback for downloads

--- helpers/versions.js
+++ helpers/versions.js
@@ -7,16 +7,13 @@ exports.load = function (app) {
 
     locals({
         neo4j:{
-            version:"1.9.RC1",
-            date:"Dec 21, 2012",
-            summary:"Cypher improvements, Gremlin refactoring",
-            readme:"http://blog.neo4j.org/2012/12/neo4j-19m03-released.html"
+            version:"2.0.0-M02", date:"Apr 22, 2013", summary:"Preview", readme:"http://blog.neo4j.org/2013/04/nodes-are-people-too.html"
         }, neo4jGA:{
-            version:"1.8.1", date:"Dec 14, 2012", summary:"General Availability", readme:"http://blog.neo4j.org/2012/12/neo4j-1-8-1-release-stability-and-cypher-performance.html"
+            version:"1.9", date:"May 20, 2013", summary:"General Availability", readme:"http://blog.neo4j.org/2012/12/neo4j-1-8-1-release-stability-and-cypher-performance.html"
         }, neo4jP:{
             version:"2.0.0-M02", date:"Apr 22, 2013", summary:"Preview", readme:"http://blog.neo4j.org/2013/04/nodes-are-people-too.html"
         }, neo4jS:{
-            version:"1.9-SNAPSHOT",
+            version:"2.0-SNAPSHOT",
             date:"2013",
             summary:"Unstable Snapshot, for resolution issue verification"
         }
