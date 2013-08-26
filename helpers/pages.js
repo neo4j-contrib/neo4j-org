84f14a8bd3442925cc64fdf06c36dcf97396ad7c
peterneubauer <peter@neubauer.se>
2013-08-26 10:34:15 +0200

    updating viz link

--- helpers/pages.js
+++ helpers/pages.js
@@ -1442,6 +1442,12 @@ exports.pages = {
                 introText: "The aim of the project is to create a lightweight 3D library with a very low level of complexity"
                 // todo max article, neo4waza etc.
             },
+            {
+                title: "A collection of graph visualization options",
+                path: "http://memit.com/mesirii",
+                introText: "Michael Hunger has a nice collection on interesting projects in the visualisation space.",
+                thumbnail: "http://d2g1zjdg4phkb4.cloudfront.net/IiVRi9fidoad7M4g5KflUrgx3Tg="
+            },
             content.articles.jdbc
         ]
     },
