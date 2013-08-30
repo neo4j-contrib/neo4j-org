d1be66137586ce5d3210478bc3c8929bc0a5f684
Chris Leishman <chris@leishman.org>
2013-08-30 17:32:39 +0200

    Add Neo4j 1.9.3 release

--- helpers/versions.js
+++ helpers/versions.js
@@ -9,7 +9,7 @@ exports.load = function (app) {
         neo4j:{
             version:"2.0.0-M04", date:"Aug 7, 2013", summary:"Preview", readme:"http://blog.neo4j.org/2013/08/summer-release-neo4j-20-milestone-4.html"
         }, neo4jGA:{
-            version:"1.9.2", date:"May 20, 2013", summary:"General Availability", readme:"http://blog.neo4j.org/2013/07/neo4j-192-now-available.html"
+            version:"1.9.3", date:"Aug 30, 2013", summary:"General Availability", readme:"http://blog.neo4j.org/2013/08/neo4j-193-now-available.html"
         }, /*neo4jP:{
             version:"2.0.0-M03", date:"May 28, 2013", summary:"Preview", readme:"http://blog.neo4j.org/2013/04/nodes-are-people-too.html"
         },*/ neo4jS:{
