a45f2fa9fd5b9e6a4e7f5b6f3803fc03f5961970
Michael Hunger <github@jexp.de>
2013-10-29 22:32:35 +0100

    added CORS headers for all objects

--- app.js
+++ app.js
@@ -225,6 +225,12 @@ app.configure(function () {
         }
         next();
     });
+    app.use(function(req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET'); // 'PUT, GET, POST, DELETE, OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'Content-Type');
+        next();
+    });
 	app.use(routes.assets);
     app.use(express.logger('dev'));
     app.use(express.bodyParser());
