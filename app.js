f1c07c69735a7e8b108ff7f45936ce824fb13df6
Michael Hunger <github@jexp.de>
2014-07-21 11:03:40 -0700

    allowing all response headers

--- app.js
+++ app.js
@@ -306,7 +306,7 @@ app.configure(function () {
     app.use(function(req, res, next) {
         res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Methods', 'GET'); // 'PUT, GET, POST, DELETE, OPTIONS');
-        res.header('Access-Control-Allow-Headers', 'Content-Type');
+        res.header('Access-Control-Allow-Headers', '*');
         next();
     });
     app.use(function(req, res, next) {
