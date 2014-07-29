6dc775cdd0fa5abbad56893f00dcf7a5c5a20e04
Michael Hunger <github@jexp.de>
2014-07-29 21:06:31 +0200

    2.1.3 release

--- helpers/versions.js
+++ helpers/versions.js
@@ -7,13 +7,13 @@ exports.load = function (app) {
 
     locals({
         neo4j:{
-            version:"2.1.2", date:"June 12, 2014", summary:"Release", readme:"http://neo4j.com/blog/neo4j-2-1-2/"
+            version:"2.1.3", date:"July 28, 2014", summary:"Release", readme:"http://neo4j.com/blog/neo4j-2-1-2/"
         }, neo4jGA:{
-            version:"2.1.2", date:"June 12, 2014", summary:"Release", readme:"http://neo4j.com/blog/neo4j-2-1-2/"
+            version:"2.1.3", date:"July 28, 2014", summary:"Release", readme:"http://neo4j.com/blog/neo4j-2-1-2/"
         }, /*neo4jP:{
             version:"2.0.0-M03", date:"May 28, 2013", summary:"Preview", readme:"http://blog.neo4j.org/2013/04/nodes-are-people-too.html"
         },*/ neo4jS:{
-            version:"2.1.3-SNAPSHOT",
+            version:"2.1.4-SNAPSHOT",
             date:"2014",
             summary:"Unstable Snapshot, for issue resolution verification"
         }
